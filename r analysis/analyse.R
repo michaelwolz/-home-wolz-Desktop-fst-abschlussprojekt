b006268591f4b99dcc3984cea2d84653ab22857b
Michael Wolz <michaelwolz93@gmail.com>
2018-03-13 20:04:36 +0100

    updated plots

--- r analysis/analyse.R
+++ r analysis/analyse.R
@@ -1,16 +1,6 @@
 # Load the packages
 library(sqldf)
 library(ggplot2)
-library(RColorBrewer)
-
-##################################
-#             HELPER             #
-##################################
-
-# This function sets the annotation count in relation to the revision count at the specific quarter
-set_relative_count <- function(annotation) {
-  # 
-}
 
 ##################################
 #           LOAD DATA            #
@@ -49,21 +39,47 @@ annotation_use = subset(annotation_use, count > 3)
 # Unique annotations
 #print(nrow(subset(filtered_annotations,!duplicated(filtered_annotations$annotation)))) # 454
 
-# Normalize data
-#lapply(filtered_annotations, set_relative_count)
 
 ##################################
 #          ANALYZE DATA          #
 ##################################
 
-# Get Top 15 annotations
+# Get Top 15 annotations by use
+top15_by_use_tmp = aggregate(list(max_count=filtered_annotations$count), by=list(annotation=filtered_annotations$annotation), max)
+top15_by_use = tail(top15_by_use_tmp[order(top15_by_use_tmp$max_count),], 15)
+
+p = sum(top15_by_use_tmp$max_count)
+
+par(mai=c(1,2,1,1))
+bp = barplot(top15_by_use$max_count / p * 100, main="Top 15 most used annotations", horiz=TRUE, names.arg=top15_by_use$annotation, las=1, xlim=c(0,56))
+text((top15_by_use$max_count / p * 100), bp, paste(round((top15_by_use$max_count / p * 100), 2), "%"), pos=4, cex=0.8)
+
+# Get Top 15 annotations by projects
 top15 = tail(annotation_use[order(annotation_use$count),], 15)
 sum_annotation_use = sum(annotation_use$count)
 par(mai=c(1,2,1,1))
-bp = barplot(top15$count / sum_annotation_use * 100, main="Top 15 most used annotations", horiz=TRUE, names.arg=top15$annotation, las=1, xlim=c(0,20))
+bp = barplot(top15$count / sum_annotation_use * 100, main="Top 15 most annotations in projects", horiz=TRUE, names.arg=top15$annotation, las=1, xlim=c(0,20))
 text((top15$count / sum_annotation_use * 100), bp, paste(round((top15$count / sum_annotation_use * 100), 2), "%"), pos=4, cex=0.8)
 
 # Look at Top 15 annotations in context of time
 top15_annotations_time = sqldf("SELECT a.annotation, a.year, a.quarter, a.count FROM filtered_annotations as a INNER JOIN top15 as t ON a.annotation = t.annotation")
+
 annotation_plot = ggplot(top15_annotations_time, aes(x=paste(substr(year, 3,4), quarter, sep="/"), y=count, group=annotation, colour=annotation)) + geom_line() + geom_point() + xlab("Year/Quarter") + ylab("% Annotation") + ggtitle("Top 15 annotations over time") + labs(color="Annotation")  
 plot(annotation_plot)
+
+top15_merge_revisions = merge(top15_annotations_time, annotation_use, by=c("annotation"), suffixes=c("Annotations", "Revision"))
+top15_merge_revisions$relativeUse = with(top15_merge_revisions, countAnnotations / countRevision * 100)
+annotation_plot3 = ggplot(top15_merge_revisions, aes(x=paste(substr(year, 3,4), quarter, sep="/"), y=relativeUse, group=annotation, colour=annotation)) + geom_line() + geom_point() + xlab("Year/Quarter") + ylab("% Annotation") + ggtitle("Top 15 annotations over time") + labs(color="Annotation")  
+plot(annotation_plot3)
+
+max_values = aggregate(list(max_count=top15_annotations_time$count), by=list(annotation=top15_annotations_time$annotation), max)
+top15_merged = merge(top15_annotations_time, max_values, by=c("annotation"))
+top15_merged$relativeUse = with(top15_merged, count / max_count * 100)
+annotation_plot4 = ggplot(top15_merged, aes(x=paste(substr(year, 3,4), quarter, sep="/"), y=relativeUse, group=annotation, colour=annotation)) + geom_line() + geom_point() + xlab("Year/Quarter") + ylab("% Annotation") + ggtitle("Top 15 annotations over time") + labs(color="Annotation")  
+plot(annotation_plot4)
+
+# Plot only deprecated 
+deprecated = sqldf("SELECT a.annotation, a.year, a.quarter, a.count FROM filtered_annotations as a WHERE a.annotation = 'Deprecated'")
+max_dep = max(deprecated$count)
+dep_plot = ggplot(deprecated, aes(x=paste(substr(year, 3,4), quarter, sep="/"), y=count / max_dep * 100, group=annotation,colour=annotation)) + geom_line() + geom_point() + xlab("Year/Quarter") + ylab("% Annotation") + ggtitle("Top 15 annotations over time") + labs(color="Annotation")  
+plot(dep_plot)
